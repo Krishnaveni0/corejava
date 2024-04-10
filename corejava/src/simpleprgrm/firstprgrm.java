1b675c008a9afe3cb58bee501fbd8513c3f841a6
Krishnaveni <Krishnaveni@DESKTOP-FA9PJ0M.Dlink>
2024-04-10 21:40:43 +0530

    krishnaveni

--- corejava/src/simpleprgrm/firstprgrm.java
+++ corejava/src/simpleprgrm/firstprgrm.java
@@ -2,15 +2,11 @@ package simpleprgrm;
 
 public class firstprgrm {
 
-
-
-public class firstprgrm {
   public   int  a=9;// data member
     private float g=9.6f;
     protected int s=5;
   
   int d=3;
     void main() { // member function
-    	
     }
 	
