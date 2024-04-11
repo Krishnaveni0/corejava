c34cf42efdcb0cbba87feaefcd3433cecbfb9b4a
Krishnaveni <Krishnaveni@DESKTOP-FA9PJ0M>
2024-04-11 19:10:57 +0530

    scanner

--- corejava/src/com/tnsif/sacnnerprogram/Personexce.java
+++ corejava/src/com/tnsif/sacnnerprogram/Personexce.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Personexce {
 
-spublic static void main(String[] args) {
+public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("enter person name");
 		String n=sc.nextLine();
