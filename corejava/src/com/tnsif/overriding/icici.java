1b59e3c21a19c00c608c62bfe8befb304b448f11
Krishnaveni <Krishnaveni@DESKTOP-FA9PJ0M.Dlink>
2024-04-21 09:42:25 +0530

    override

---
+++ corejava/src/com/tnsif/overriding/icici.java
@@ -0,0 +1,11 @@
+package com.tnsif.overriding;
+
+public class icici extends Rbi {
+		@Override
+		public float getRateofInterest() {
+				return 5.6f;
+	}
+
+}
+
+
---
+++ corejava/src/com/tnsif/superkeyword/Supervariable.java
@@ -0,0 +1,10 @@
+package com.tnsif.superkeyword;
+
+public class Supervariable {
+
+		int a=9;
+		
+		void drink() {
+				System.out.println("tea");
+		}
+}
